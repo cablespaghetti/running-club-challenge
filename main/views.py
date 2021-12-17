fae4b3ad23e476384904d1699313e2285fa8432f
Sam Weston <sam.weston@tails.com>
2021-12-17 23:29:02 +0000

    Deal with different tables for men and women

--- main/views.py
+++ main/views.py
@@ -9,15 +9,15 @@ def race_results(request, race_id):
     race = get_object_or_404(Race, id=race_id)
 
     activities = Activity.objects.filter(race=race).order_by('elapsed_time')
-    fastest_per_athlete_activities = []
+    results_dict = {'F': [], 'M': []}
     processed_athlete_list = []
     for activity in activities:
         if activity.athlete not in processed_athlete_list:
-            fastest_per_athlete_activities.append(activity)
+            results_dict[activity.athlete.gender].append(activity)
             processed_athlete_list.append(activity.athlete)
     template_context = {
         'race_name': race.name,
-        'results_list': fastest_per_athlete_activities
+        'results_dict': results_dict,
     }
     return render(request, 'main/race_results.html', template_context)
 
