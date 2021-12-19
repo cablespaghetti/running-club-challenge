1f6a03316065ea9dda6eec2f58a551ae05ed02aa
Sam Weston <sam.weston@tails.com>
2021-12-19 15:39:20 +0000

    Add missing files and make evidence upload mandatory

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, FileField
 from main.models import Activity
 
 
@@ -6,3 +6,7 @@ class SubmitActivityForm(ModelForm):
     class Meta:
         model = Activity
         fields = ['race', 'start_time', 'elapsed_time', 'evidence_file']
+
+    def __init__(self, *args, **kwargs):
+        super(SubmitActivityForm, self).__init__(*args, **kwargs)
+        self.fields['evidence_file'] = FileField(required=True)
