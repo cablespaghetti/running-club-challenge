33c659337c0576fd4fce234d26ad989a4d1f0211
Sam Weston <sam.weston@tails.com>
2021-12-23 16:41:07 +0000

    Stop stravalib logging endlessly

--- challenge/settings.py
+++ challenge/settings.py
@@ -162,15 +162,29 @@ SOCIALACCOUNT_AUTO_SIGNUP = True
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {name} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
         },
     },
     'root': {
         'handlers': ['console'],
         'level': 'INFO',
     },
+    'loggers': {
+        'stravalib.protocol.ApiV3': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARNING'
+        }
+    }
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
